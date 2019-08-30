53d3804f1fef9de9073acceaaa61076c589395ed
Kav Singh <flakisms@gmail.com>
2019-08-30 22:27:04 +0200

    remove minification

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,17 @@
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
 export default {
   input: './src/index.ts',
   output: [
-    { file: './dist/index.js', format: 'cjs', sourcemap: true },
-    { file: './dist/index.module.js', format: 'es', sourcemap: true },
+    { file: './dist/index.js', format: 'cjs' },
+    { file: './dist/index.module.js', format: 'es' },
   ],
   plugins: [
     resolve({ extensions }),
     babel({ extensions, include: ['src/**/*'], exclude: 'node_modules/**' }),
-    terser({ sourcemap: true }),
   ],
   external: ['react', 'react-native'],
 };
